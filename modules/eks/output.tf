b9e2f7f7728891e07bf34d727614cf2648c3fd9d
ashley <nguyenthanhcong101096@gmail.com>
2021-05-31 20:05:55 +0700

    k8s

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,7 +1,7 @@
 output "eks-endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+  value = aws_eks_cluster.main.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+  value = aws_eks_cluster.main.certificate_authority[0].data
 }
